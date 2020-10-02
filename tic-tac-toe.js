a4cfee95d00085d680a0e5d93e0eb8f1ec6597c0
Olga Thompson <olgatj2007@gmail.com>
2020-10-02 16:42:43 -0400

    added checkGameStatus function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,59 @@
 let currentPlayerSymbol = "x";
 const squareValues = ["", "", "", "", "", "", "", "", ""];
+let gameStatus = '';
+// const checkGameStatus = function() {
+function checkGameStatus() {
+    //check the rows
+    for (let i = 0; i < 9; i+=3) {
+        if (squareValues[i] !== ''
+        && squareValues[i] === squareValues[i + 1]
+        && squareValues[i] === squareValues[i + 2]) {
+            gameStatus = squareValues[i];
+            break;
+        }
+    }
+    //check the columns
+    for (let i = 0; i < 3; i++) {
+        if (squareValues[i] !== ''
+        && squareValues[i] === squareValues[i + 3]
+        && squareValues[i] === squareValues[i + 6]) {
+            gameStatus = squareValues[i];
+            break;
+        }
+    }
+
+    //check the diagnols;
+    if (squareValues[0] !== ''
+    && squareValues[0] === squareValues[4]
+    && squareValues[0] === squareValues[8]) {
+        gameStatus = squareValues[0];
+    }
+    if (squareValues[2] !== ''
+    && squareValues[2] === squareValues[4]
+    && squareValues[2] === squareValues[6]) {
+        gameStatus = squareValues[2];
+    }
+//check for tie
+let boardIsFilled = true;
+for (let i = 0; i < 9; i += 1){
+if(squareValues[i] === '') {
+boardIsFilled=false;
+break;
+}
+}
+if (boardIsFilled) {
+gameStatus ='None';
+}
+
+
+    if (gameStatus != '') {
+        document
+        .getElementById('game-status')
+        .innerHTML = `Winner is: ${gameStatus.toUpperCase()}`
+.getElementById('new-game').disable = false;
+    }
+
+}
 
 window.addEventListener("DOMContentLoaded", (event) => {
 document.getElementById("tic-tac-toe-board")
@@ -25,8 +79,18 @@ document.addEventListener("click", event => {
     } else {
         currentPlayerSymbol = "x";
     }
-
+    checkGameStatus();
 })
+document.getElementById("new-game").addEventListener("click", event => {
+    currentPlayerSymbol = "x";
+    squareValues = ["", "", "", "", "", "", "", "", ""];
+    gameStatus = '';
+    document
+        .getElementById('game-status')
+        .innerHTML = '';
+});
+
+
 
 
 });
